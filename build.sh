372b8f02052f03a5c6b06a937ec77016a737b8ab
Iku Iwasa <iku.iwasa@gmail.com>
2018-04-30 22:28:58 +0900

    Extract /usr/share/mk from base.tgz

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ curl $NETBSD_URL/source/sets/gnusrc.tgz | tar xzf -
 curl $NETBSD_URL/source/sets/sharesrc.tgz | tar xzf -
 curl $NETBSD_URL/source/sets/syssrc.tgz | tar xzf -
 
-curl $NETBSD_URL/i386/binary/sets/base.tgz | tar xzf - -C /x-tools/i386-unknown-netbsd/sysroot ./usr/include ./usr/lib ./lib
+curl $NETBSD_URL/i386/binary/sets/base.tgz | tar xzf - -C /x-tools/i386-unknown-netbsd/sysroot ./usr/include ./usr/lib ./lib ./usr/share/mk
 curl $NETBSD_URL/i386/binary/sets/comp.tgz | tar xzf - -C /x-tools/i386-unknown-netbsd/sysroot ./usr/include ./usr/lib
 
 cd usr/src
